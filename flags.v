b135b2eee9ff4305c1988ca7afbf1b90917a22f0
Chase McCarthy <chase@code0100fun.com>
2022-09-04 10:49:51 -0500

    fix windows msys2 flags

--- flags.v
+++ flags.v
@@ -1,7 +1,15 @@
 module glfw3
 
 #flag -DGLFW_INCLUDE_NONE
-#flag windows -lglfw3
-#flag linux -lglfw
 
-#include <GLFW/glfw3.h>
+$if windows {
+    // These hard coded paths should not be necessary.
+    // This initially worked without a full path.
+    // ¯\_(ツ)_/¯
+    #flag -LC:/msys64/mingw64/bin/
+    #flag -lglfw3
+    #include "C:/msys64/mingw64/include/GLFW/glfw3.h"
+} $else {
+    #flag -lglfw
+    #include "GLFW/glfw3.h"
+}
